185813d6f8ef9f97156e42dbc1b7ac88195532b2
Nic <limn4@student.op.ac.nz>
2020-09-25 14:16:23 +1200

    trying forms too

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -23,6 +23,13 @@ export default function Contact() {
   )
 }
 
+<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
+</form>
+
+
 <script type="text/babel">
 
       
