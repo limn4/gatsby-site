fe930dd93b2486eacfc055007230b190c1d010cd
Nic <limn4@student.op.ac.nz>
2020-09-25 13:49:04 +1200

    forms?

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,6 +10,28 @@ export default function Contact() {
         <Link to="/about/">About</Link>
         <p></p>
         <Link to="/posts/">Posts</Link>
-    </div>
+    
+    <script type="text/babel">
+
+    
+      <form name="contact" method="post">
+        <p>
+          <label>Your Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email"/></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>,
+      
+    
+
+  </script>
+  </div>
   )
 }
