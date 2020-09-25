4b5b0287cca5f983192668d4bd56a27a35216543
Nic <limn4@student.op.ac.nz>
2020-09-25 14:04:36 +1200

    forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,27 +18,29 @@ export default function Contact() {
       </form>
 
 
-      <script type="text/babel">
-
       
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>,
-     
-
-      </script>
     </div>
   )
 }
+
+<script type="text/babel">
+
+      
+<form name="contact" method="post">
+  <input type="hidden" name="form-name" value="contact" />
+  <p>
+    <label>Your Name: <input type="text" name="name"/></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email"/></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+
+</script>
