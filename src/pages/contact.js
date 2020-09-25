ee244f40e6eef35da7527702733406ef463e7768
Nic <limn4@student.op.ac.nz>
2020-09-25 13:54:19 +1200

    forms?

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,34 +4,42 @@ import { Link } from "gatsby"
 export default function Contact() {
   return (
     <div>
-        <h1>Contact Page</h1>
-        <Link to="/">Index</Link>
-        <p></p>
-        <Link to="/about/">About</Link>
-        <p></p>
-        <Link to="/posts/">Posts</Link>
+      <h1>Contact Page</h1>
+      <Link to="/">Index</Link>
+      <p></p>
+      <Link to="/about/">About</Link>
+      <p></p>
+      <Link to="/posts/">Posts</Link>
     
-    <script type="text/babel">
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name"/>
+        <input type="email" name="email"/>
+        <textarea name="message"></textarea>
+      </form>
 
-    
-      <form name="contact" method="post">
-        <p>
-          <label>Your Name: <input type="text" name="name"/></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email"/></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>,
-      
-    
 
-  </script>
-  </div>
+      <script type="text/babel">
+
+      ReactDOM.render(
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>,
+        document.getElementById("root")
+      );
+
+      </script>
+    </div>
   )
 }
