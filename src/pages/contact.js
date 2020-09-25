5d4b5c06b2dd150c9e96edfaee4f71be7e3e461f
Nic <limn4@student.op.ac.nz>
2020-09-25 14:19:49 +1200

    trying forms two

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,43 +11,27 @@ export default function Contact() {
       <p></p>
       <Link to="/posts/">Posts</Link>
     
-      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-        <input type="text" name="name"/>
-        <input type="email" name="email"/>
-        <textarea name="message"></textarea>
-      </form>
-
-
+      
+      <form name="contact" method="post" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>Your Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email"/></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+        </form>
       
     </div>
   )
 }
 
-<form name="contact" netlify netlify-honeypot="bot-field" hidden>
-  <input type="text" name="name" />
-  <input type="email" name="email" />
-  <textarea name="message"></textarea>
-</form>
 
 
-<script type="text/babel">
 
-      
-<form name="contact" method="post">
-  <input type="hidden" name="form-name" value="contact" />
-  <p>
-    <label>Your Name: <input type="text" name="name"/></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email"/></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
-
-</script>
